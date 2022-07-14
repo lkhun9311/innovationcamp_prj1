23f0e74908fe7a11716ca05939a7d760c64d3ea0
Moveuk <v_donguk@naver.com>
2022-07-14 15:11:23 +0900

    fix: 좋아요 버튼 기능 js.num2str() 수정

--- static/myjs.js
+++ static/myjs.js
@@ -144,3 +144,16 @@ function toggle_like(cafe_id, type) {
     }
 }
 
+
+function num2str(count) {
+    if (count > 10000) {
+        return parseInt(count / 1000) + "k"
+    }
+    if (count > 500) {
+        return parseInt(count / 100) / 10 + "k"
+    }
+    if (count == 0) {
+        return ""
+    }
+    return count
+}
